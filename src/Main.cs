94cf1c1e6f0a97b669bade93cca883ad0f59b539
HuseynliFarid <fridyusfinhuseynli@gmail.com>
2026-07-07 15:26:16 +0400

    addOOPTasks

---
+++ src/Main.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace OOPTASKS
+{
+   
+    class MainOOPTASKS
+    {
+        static void Main(string[] args)
+        {
+            // Create an instance of the Book class
+            Book book = new Book("The Great Gatsby", "F. Scott Fitzgerald", 180);
+
+            //Store 
+            Product product = new Product();
+            product._price = 25.99; // Set the price using the property
+            product._price = -10; // Attempt to set a negative price (will be set to 0)
+            Console.WriteLine(product._price);
+
+        }
+    }
+}
---
+++ src/Store.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPTASKS
+{
+    internal class Product
+    {
+        private double price {  get; set; }
+
+        public double _price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                if(value >= 0)
+                {
+                    price = value;
+                }
+                else
+                {
+                    price = 0;
+                }
+            }
+        }
+    }
+} 
+
