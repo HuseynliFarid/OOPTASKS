d739f30293d0e517b8662afea8e335af89af398c
HuseynliFarid <fridyusfinhuseynli@gmail.com>
2026-07-07 19:19:26 +0400

    UpdatedCtor

---
+++ src/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPTASKS.src
+{
+    public class Student
+    {
+        public string Name;
+        private byte _point;
+        
+        public byte Point
+        {
+            get { return _point; }
+            set
+            {
+                if (value < 0 || value > 100)
+                {
+                    Console.WriteLine("Point must be between 0 and 100.");
+                }
+                else _point = value;
+            }
+        }
+
+    }
+}
